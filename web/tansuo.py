e520cb8be648c9efa8e4aa4ec5fe9d33deead6a8
SolaKami <674564721@qq.com>
2018-04-03 21:59:22 +0800

    1.5.4

--- web/tansuo.py
+++ web/tansuo.py
@@ -2,12 +2,15 @@ from robot import tansuorobot
 import win32gui
 import time
 
-hwnd = win32gui.FindWindow(0,"阴阳师-网易游戏")
-win32gui.SetForegroundWindow(hwnd)
+try:
+    hwnd = win32gui.FindWindow(0, "阴阳师-网易游戏")
+    win32gui.SetForegroundWindow(hwnd)
+except:
+    pass
 
 time.sleep(3)
 
-robot1 = tansuorobot.TanSuoRobot(100, 16)
+robot1 = tansuorobot.TanSuoRobot(100, 24)
 
 # robot1.start()
 robot1.tansuo()
